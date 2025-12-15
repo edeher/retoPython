9f97ffe0140b8dd98a6bd841f6e9c90b6cca2fca
dekinha <edeher_deka@hotmail.com>
2025-12-15 02:14:07 -0500

    day 9

--- Dia 9/1-Mod_collections.py
+++ Dia 9/1-Mod_collections.py
@@ -1,3 +1,16 @@
-from collections import Counter
+from collections import Counter, defaultdict
 numeros = [8, 6, 9, 5, 4, 5, 5, 8, 7, 4, 5, 4, 4]
-print(Counter(numeros))  # Conta a frequência de cada número na lista
+print(Counter(numeros)) 
+
+print(Counter("mississippi"))
+
+frase = 'al pan pan y al vino vino'
+
+print(Counter(frase.split()))
+
+serie = Counter([1, 1, 2, 3, 4, 4, 4, 5, 5, 5, 5])
+print(serie.most_common(1))
+print(serie.most_common(3))
+print(list(serie))
+# Conta a frequência de cada número na lista
+print('-----------------------------------')
